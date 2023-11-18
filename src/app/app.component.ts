7e748ee8e0208c18b6072880dfc65f4c4004baba
Balbona Mickael <balbona.mickael@gmail.com>
2023-11-18 11:11:41 +0100

    M: app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
+import { AboutComponent } from "./about/about.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent]
+    imports: [CommonModule, AboutComponent]
 })
 export class AppComponent {
   title = 'ng-portfolio';
