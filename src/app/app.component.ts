3a9f1dc247bc9a0ebfd3f0a99ab1b18fc260e982
Balbona Mickael <balbona.mickael@gmail.com>
2023-11-20 11:34:55 +0100

    Add:  new routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from "./about/about.component";
+import { ResumeComponent } from "./resume/resume.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent, AboutComponent, ResumeComponent, PortfolioComponent, ContactComponent]
 })
 export class AppComponent {
   title = 'ng-portfolio';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 
-
 export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'resume', component: ResumeComponent },
     { path: 'portfolio', component: PortfolioComponent },
     { path: 'contact', component: ContactComponent }
 ];
